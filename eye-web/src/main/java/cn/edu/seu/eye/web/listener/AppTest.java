50621707156d0b05470e0c4d96ed570566452805
金成 <kingjci@sina.com>
2014-12-04 20:42:44 +0800

    add somthings

--- eye-web/src/main/java/cn/edu/seu/eye/web/listener/AppTest.java
+++ eye-web/src/main/java/cn/edu/seu/eye/web/listener/AppTest.java
@@ -1,11 +1,17 @@
 package cn.edu.seu.eye.web.listener;
 
+import cn.edu.seu.eye.web.AppConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
 /**
  * Created by sean on 14/10/21.
  */
 public class AppTest {
 
     public static void main(String[] args) {
+        System.out.println("hello world");
+
 
     }
 }
